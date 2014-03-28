900d858e9858cec1831d35a29f30878b6f6ee513
Jonathan E. Magen <yonkeltron@gmail.com>
2014-03-28 17:10:03 -0400

    campus factory

---
+++ lib/import/campus_factory.rb
@@ -0,0 +1,17 @@
+class CampusFactory
+  include ActiveModel::Model
+  include Contracts
+
+  attr_accessor :campus_id, :name, :institution_id, :address, :city, :state, :zip
+
+  Contract nil => Campus
+  def make
+    Campus
+      .where(campus_id: campus_id,
+             institution_id: institution_id)
+      .first_or_initialize(name: name,
+                           address: address,
+                           city: city,
+                           state: state)
+  end
+end
---
+++ spec/lib/import/campus_factory_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe CampusFactory do
+  let(:campus) { FactoryGirl.build(:campus) }
+
+  let(:attributes) do
+    {
+      :name           => campus.name,
+      :campus_id      => campus.campus_id,
+      :institution_id => campus.institution_id,
+      :address        => campus.address,
+      :city           => campus.city,
+      :state          => campus.state,
+      :zip            => campus.zip
+    }
+  end
+
+  let(:campus_factory) { CampusFactory.new(attributes) }
+
+  describe '#make' do
+    context 'given no existing Campus' do
+      it 'Campus' do
+        expect { campus_factory.make.save! }.to change(Campus, :count).by(1)
+      end
+    end
+
+    context 'given an existing Campus' do
+      it 'Campus' do
+        campus.save!
+        expect { campus_factory.make.save! }.to change(Campus, :count).by(0)
+      end
+    end
+  end
+end
