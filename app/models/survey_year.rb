b6aacd19b04ad23da386a3cb9531533ef81e4f4f
Jonathan E. Magen <yonkeltron@gmail.com>
2014-03-31 17:36:16 -0400

    annotate models

--- app/models/survey_year.rb
+++ app/models/survey_year.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: survey_years
+#
+#  id          :integer          not null, primary key
+#  name        :integer
+#  campus_id   :integer
+#  men_total   :integer
+#  women_total :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class SurveyYear < ActiveRecord::Base
   belongs_to :campus
 
--- spec/factories/survey_years.rb
+++ spec/factories/survey_years.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: survey_years
+#
+#  id          :integer          not null, primary key
+#  name        :integer
+#  campus_id   :integer
+#  men_total   :integer
+#  women_total :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
