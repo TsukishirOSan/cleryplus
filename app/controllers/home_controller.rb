c05c8a5522efa730ef89d8510b854947e5f002a8
Jonathan E. Magen <yonkeltron@gmail.com>
2014-04-07 16:33:52 -0400

    redirect to s3-hosted site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    render text: "I think you want to learn more about me here: https://github.com/SexualHealthInnovations/cleryplus", status: 418
+    redirect_to 'https://shifiles.s3.amazonaws.com/cleryplus/index.html'
   end
 end
