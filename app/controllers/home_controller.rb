6239dc19e3eb5f9c12cd2b6854a9dc0d1c7b10c0
Jonathan E. Magen <yonkeltron@gmail.com>
2014-04-10 10:42:03 -0400

    changing index of home to 303
    Signed-off-by: Jonathan E. Magen <yonkeltron@gmail.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
   def index
-    redirect_to 'https://cleryplus.s3.amazonaws.com/index.html'
+    redirect_to 'https://cleryplus.s3.amazonaws.com/index.html', status: 303
   end
 end
